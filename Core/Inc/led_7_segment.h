8350650bfc26cd9cd0b7170107de6ea6a01da8eb
KhiemNC <nguyencongkhiem510@gmail.com>
2024-10-21 13:52:09 +0700

    Add 3 more function to display better,. Especially, I modified the display_a_digit() function, the led behavior don't flash anymore.

--- Core/Inc/led_7_segment.h
+++ Core/Inc/led_7_segment.h
@@ -11,6 +11,9 @@
 #include "main.h"
 #include "common.h"
 
+void set_on_4digits();
+void unable_4digit();
+void display_4digits();
 void update_led_7seg_buffer();
 void display_a_digit(int digit, int num);
 void display_number(int num);
--- Core/Src/led_7_segment.c
+++ Core/Src/led_7_segment.c
@@ -7,6 +7,36 @@
 
 #include "led_7_segment.h"
 
+void set_on_4digits()
+{
+	status_4digits[0] = LED_ON;
+	status_4digits[1] = LED_ON;
+	status_4digits[2] = LED_ON;
+	status_4digits[3] = LED_ON;
+}
+
+void unable_4digit() {
+	HAL_GPIO_WritePin(EN_0_GPIO_Port, EN_0_Pin, DISABLE);
+	HAL_GPIO_WritePin(EN_1_GPIO_Port, EN_1_Pin, DISABLE);
+	HAL_GPIO_WritePin(EN_2_GPIO_Port, EN_2_Pin, DISABLE);
+	HAL_GPIO_WritePin(EN_3_GPIO_Port, EN_3_Pin, DISABLE);
+}
+
+void display_4digits() {
+	if ((status_4digits[led_7seg_index] == LED_ON)
+			&& ((led_7seg_index == 1 && led_7seg_buffer[0])
+					|| (led_7seg_index == 3 && led_7seg_buffer[2])
+					|| led_7seg_buffer[led_7seg_index]))
+	{
+		display_a_digit(led_7seg_index, led_7seg_buffer[led_7seg_index]);
+	}
+	++led_7seg_index;
+	if (led_7seg_index > 3)
+	{
+		led_7seg_index = 0;
+	}
+}
+
 void update_led_7seg_buffer()
 {
 	led_7seg_buffer[0] = (counter_way0 / 10) % 10;
@@ -17,8 +47,10 @@ void update_led_7seg_buffer()
 
 void display_a_digit(int digit, int num)
 {
-	enable_digit(digit);
+	unable_4digit();
 	display_number(num);
+	enable_digit(digit);
+
 }
 
 void display_number(int num)
